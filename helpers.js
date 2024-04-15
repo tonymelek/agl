e05e5d3583ea9b63108dce8890cb5f51a170068c
Tony Melek <tonymelek.au@gmail.com>
2024-04-15 21:30:28 +1000

    add dodo comparison

--- helpers.js
+++ helpers.js
@@ -6,6 +6,11 @@ const tariffs = {
     "feedInFirst10": 0.1,
     "feedInNormal": 0.049
 };
+const dodo_tariffs={
+    usageRate:0.3517,
+    dailySupply: 0.5687,
+    solarRate:0.049
+}
 
 
 let periodInDays = 0;
@@ -23,6 +28,8 @@ const calculateSolar = (solarExport) => {
 }
 const totalDailyCost = () => {
     const consumption = usage.filter(item => item.RateTypeDescription === "Usage");
+    const solarExport = usage.filter(item => item.RateTypeDescription === "Solar");
+
     const peakConsumption = consumption.filter(item => {
         const hr = parseInt(item.StartDate.split(' ')[1].split(':')[0]);
         const isPM = /pm/i.test(item.StartDate);
@@ -35,12 +42,17 @@ const totalDailyCost = () => {
         const hr24 = isPM ? hr + 12:hr;
         return hr24 < tariffs.peakHours.start || hr24 > tariffs.peakHours.end;
     });
-    const solarExport = usage.filter(item => item.RateTypeDescription === "Solar");
     const solarExportCost = calculateSolar(solarExport);
     const peakConsumptionCost = calculateCost(peakConsumption, tariffs.peakRate);
     const offPeakConsumptionCost = calculateCost(offPeakConsumption, tariffs.normalRate);
     const supplyCharge = tariffs.dailySupply * periodInDays;
     const netCost = peakConsumptionCost + offPeakConsumptionCost + supplyCharge - solarExportCost;
+
+    const dodo_usage=calculateCost(consumption,dodo_tariffs.usageRate);
+    const dodo_solar=calculateCost(solarExport,dodo_tariffs.solarRate);
+    const dodo_supply=dodo_tariffs.dailySupply*periodInDays;
+    const dodo_net=dodo_supply+dodo_usage-dodo_solar;
+
     document.querySelector('tr#results').innerHTML = `
     <td>${periodInDays}</td>
     <td>$${solarExportCost.toFixed(2)}</td>
@@ -48,6 +60,13 @@ const totalDailyCost = () => {
     <td>$${offPeakConsumptionCost.toFixed(2)}</td>
     <td>$${supplyCharge.toFixed(2)}</td>
     <td>$${netCost.toFixed(2)}</td>`;
+
+    document.querySelector('tr#dodo_results').innerHTML = `
+    <td>${periodInDays}</td>
+    <td>$${dodo_solar.toFixed(2)}</td>
+    <td>$${dodo_usage.toFixed(2)}</td>
+    <td>$${dodo_supply.toFixed(2)}</td>
+    <td>$${dodo_net.toFixed(2)}</td>`;
 
     console.log("Usage: $", (peakConsumptionCost + offPeakConsumptionCost).toFixed(2), "Solar: $", calculateSolar(solarExport).toFixed(2), "Supply charge:", (tariffs.dailySupply * periodInDays).toFixed(2));
     console.log("Total Payable: $", (peakConsumptionCost + offPeakConsumptionCost - calculateSolar(solarExport) + tariffs.dailySupply * periodInDays).toFixed(2));
